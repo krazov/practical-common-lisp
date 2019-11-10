484627abbd29b271c729f2e675f8bc60876aa115
Jon Krazov <krazov@interia.pl>
2019-11-10 20:07:27 +0100

    Fixing how code is formatted (due to tabs and not spaces in LispIDE)

--- 003_practical-database.lisp
+++ 003_practical-database.lisp
@@ -38,9 +38,10 @@
 
 ;; disk operations
 (defun save-db (filename)
-	(with-open-file (out filename
-					 :direction :output
-					 :if-exists :supersede)
+	(with-open-file 
+			(out filename
+			:direction :output
+			:if-exists :supersede)
 		(with-standard-io-syntax
 			(print *db* out))))
 
