8d774664f407dd868af4a669685bc02b4aad6c59
Jon Krazov <krazov@interia.pl>
2019-11-17 13:59:43 +0100

    [own] Todo: UI and status update
    * Adding generic prompt-read\n* Creating a todo from prompt\n* main loop that inquires about todo and asks if another should be added, as well\n* find-by-id function that confirms a todo by its id\n* set-done function that allows to update todo's status by id

--- 004_own-todo.lisp
+++ 004_own-todo.lisp
@@ -2,7 +2,11 @@
 
 (defparameter *new-id*
     (let ((count 0))
-        #'(lambda () (setf count (1+ count)))))
+        #'(lambda () (setq count (1+ count)))))
+
+(defun find-by-id (id)
+    #'(lambda (todo)
+        (equal (getf todo :id) id)))
 
 (defun new-todo (task)
     (list 
@@ -12,3 +16,27 @@
 
 (defun add-todo (todo)
     (push todo *todos*))
+
+(defun set-done (id status)
+    (setf *todos*
+        (mapcar
+            #'(lambda (todo)
+                (when (funcall (find-by-id id) todo) (setf (getf todo :done) status))
+                todo)
+            *todos*)))
+
+;;; I/O
+
+(defun prompt-read (prompt)
+    (format *query-io* "~a" prompt)
+    (force-output *query-io*)
+    (read-line *query-io*))
+
+(defun prompt-for-todo ()
+    (new-todo (prompt-read "Name your task: ")))
+
+;;; fire!
+
+(defun main ()
+    (loop (add-todo (prompt-for-todo))
+        (if (not (y-or-n-p "Do you want to add another task? [y/n]: ")) (return))))
