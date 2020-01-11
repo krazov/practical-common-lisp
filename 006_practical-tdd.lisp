c62c72e2d28ce61741f4c12b24907c9fc9278483
Jon Krazov <krazov@interia.pl>
2020-01-11 18:21:46 +0100

    Unit test practical: Iteration 3c

--- 006_practical-tdd.lisp
+++ 006_practical-tdd.lisp
@@ -17,7 +17,25 @@
 (defun report-result (result form)
     (format t "~:[FAIL~;pass~] ... ~a~%" result form))
 
-(defun test-+ ()
+(defmacro check-iteration-3b (form)
+    `(report-result ,form ',form))
+
+(defmacro check (&body forms)
+    `(progn
+        ,@(loop for form in forms collect `(report-result ,form ',form))))
+
+(defun test-+-iteration-3a ()
     (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3))
     (report-result (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
     (report-result (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+
+(defun test-+-iteration-3b ()
+    (check (= (+ 1 2) 3))
+    (check (= (+ 1 2 3) 6))
+    (check (= (+ -1 -3) -4)))
+
+(defun test-+ ()
+    (check
+        (= (+ 1 2) 3)
+        (= (+ 1 2 3) 6)
+        (= (+ -1 -3) -4)))
