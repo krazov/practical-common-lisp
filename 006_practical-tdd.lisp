5eae1cf3bb0a6ece414f51181f515d98a9c01a69
Jon Krazov <krazov@interia.pl>
2020-01-11 18:02:57 +0100

    Unit test practical: Iteration 2

--- 006_practical-tdd.lisp
+++ 006_practical-tdd.lisp
@@ -1,7 +1,12 @@
 ;;;; excercise: http://www.gigamonkeys.com/book/practical-building-a-unit-test-framework.html
 
-(defun test-+ ()
+(defun test-+-iteration-1 ()
     (and
         (= (+ 1 2) 3)
         (= (+ 1 2 3) 6)
         (= (+ -1 -3) -4)))
+        
+(defun test-+ ()
+    (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2) 3) '(= (+ 1 2) 3))
+    (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
+    (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
