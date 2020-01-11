ccc717418a86de7180ee71e21f49eba93f19760d
Jon Krazov <krazov@interia.pl>
2020-01-11 18:31:15 +0100

    Unit test practical: Reorganising iterations

--- 006_practical-tdd.lisp
+++ 006_practical-tdd.lisp
@@ -14,6 +14,27 @@
     (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
 
 ;;; iteration 3
+(defun report-result-iteration-3 (result form)
+    (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+
+(defun test-+-iteration-3 ()
+    (report-result-iteration-3 (= (+ 1 2) 3) '(= (+ 1 2) 3))
+    (report-result-iteration-3 (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
+    (report-result-iteration-3 (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+
+;;; iteration 4
+(defun report-result-iteration-4 (result form)
+    (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+
+(defmacro check-iteration-4 (form)
+    `(report-result-iteration-4 ,form ',form))
+
+(defun test-+-iteration-4 ()
+    (check-iteration-4 (= (+ 1 2) 3))
+    (check-iteration-4 (= (+ 1 2 3) 6))
+    (check-iteration-4 (= (+ -1 -3) -4)))
+
+;;; iteration 5
 (defun report-result (result form)
     (format t "~:[FAIL~;pass~] ... ~a~%" result form))
 
@@ -23,11 +44,6 @@
 (defmacro check (&body forms)
     `(progn
         ,@(loop for form in forms collect `(report-result ,form ',form))))
-
-(defun test-+-iteration-3a ()
-    (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3))
-    (report-result (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
-    (report-result (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
 
 (defun test-+-iteration-3b ()
     (check (= (+ 1 2) 3))
